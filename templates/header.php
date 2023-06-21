79598fff255f443cc8e5eca9fb2ee21fcca40bca
PN <pn@klapproth-koch.de>
2023-06-21 08:04:17 +0200

    Header

---
+++ templates/header.php
@@ -0,0 +1,11 @@
+<div class="header-inside">
+    <div class="header">
+        <div class="header-left">
+            {{insert_content::3}}
+        </div>
+        <div class="header-right">
+            {{insert_module::1}}
+        </div>
+    </div>
+</div>
+
