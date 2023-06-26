ae62a3f31bba5599c55ee591c68589210972ce3d
PN <pn@klapproth-koch.de>
2023-06-26 08:36:37 +0200

    Unterseite (karriere)

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <div class="footer-top">
+        {{insert_content::55}}
+    </div>
+    <div class="footer-middle">
+        {{insert_module::10}}
+    </div>
+    <div class="footer-bottom">
+        {{insert_module::11}}
+    </div>
+</div>
