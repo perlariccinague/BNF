061aaa7145d14af10cbf28042a8d380c8d1a3b56
PN <pn@klapproth-koch.de>
2023-10-11 08:12:12 +0200

    Mobile Styling, QM

---
+++ templates/rsce_my_box_mobile_config.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+    'label' => array('Box Mobile', 'Meine Beschreibung...'),
+    'types' => array('content', 'module'),
+    'contentCategory' => 'texts',
+    'moduleCategory' => 'miscellaneous',
+    'beTemplate' => 'be_wildcard',
+    'standardFields' => array('headline', 'cssID'),
+    'wrapper' => array(
+        'type' => 'none',
+    ),
+    'fields' => array(
+        'text' => array(
+            'label' => array('Title', 'Beschreibung...'),
+            'eval' => array('rte' => 'tinyMCE'),
+            'inputType' => 'textarea',
+
+        ),
+        'icon' => array(
+            'label' => array('Icon', 'Beschreibung...'),
+            'eval' => array('filesOnly' => true),
+            'inputType' => 'fileTree'
+        ),
+
+    ),
+);
